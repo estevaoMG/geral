f737760cf95f59563407cfa5c80f21858f339267
estevaoMG <estevao.gouveia@hotmail.com>
2022-03-08 16:26:52 -0300

    Adding Typescript and improving the way to develop.

--- projeto_REST/src/index.ts
+++ projeto_REST/src/index.ts
@@ -1,2 +1,12 @@
 
-console.log('Executei!');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'Sucesso total! Vocês são demais!' });
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação executado na porta 3000!');
+})
