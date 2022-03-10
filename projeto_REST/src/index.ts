da93b66eb3774cae95867c6e9168bbf01f944ac0
estevaoMG <estevao.gouveia@hotmail.com>
2022-03-10 12:36:27 -0300

    Developing and testing the post method.

--- projeto_REST/src/index.ts
+++ projeto_REST/src/index.ts
@@ -4,12 +4,19 @@ import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Configurações de Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'Sucesso!' });
 });
 
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executado na porta 3000!');
 })
