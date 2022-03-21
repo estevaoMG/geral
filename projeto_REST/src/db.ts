e165e0d3b7c51511397b9fff1cb346e8ad64d32a
estevaoMG <estevao.gouveia@hotmail.com>
2022-03-21 19:52:24 -0300

    Creating the basic structure for database connection.

--- projeto_REST/src/db.ts
+++ projeto_REST/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = '';
+const connectionString = 'postgres://rzjhakdn:npI4fkVXKKWt7RyMOZQJ6fLiCR6GC_BF@tuffi.db.elephantsql.com/rzjhakdn';
 
 const db = new Pool({ connectionString });
 
